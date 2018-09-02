f8c7401681a78cbab80e9288da70917d7c14a81a
rudiq4 <rudikvovan@gmail.com>
2018-09-02 14:23:54 +0300

    Final ver. without pagination

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from django.core.paginator import Paginator
 
 
 def ProductList(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    current_page = Paginator(products, 9)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
