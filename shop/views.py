6eb593fd56be716e60596ddfacda7213acdcefd0
rudiq4 <rudikvovan@gmail.com>
2018-09-02 16:25:27 +0300

    + Paginator (Backend) Front doesn't work . :/

--- shop/views.py
+++ shop/views.py
@@ -1,22 +1,50 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
 from .models import Category, Product
 from cart.forms import CartAddProductForm
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+def t_list(request):
+    products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    template = 'shop/product/list.html'
+    context = {
+        'products': products,
+        'page': page
+    }
+    return render(request, template ,context)
 
 
 def ProductList(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
-    current_page = Paginator(products, 9)
+    paginator = Paginator(products, 3)
+    page = request.GET.get('page')
+    template = 'shop/product/list.html'
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html', {
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    context = {
         'category': category,
         'categories': categories,
-        'products': products
-    })
+        'products': products,
+        'page': page
+    }
+    return render(request, template, context)
 
 
 def ProductDetail(request, id, slug):
